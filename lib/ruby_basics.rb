b89db388a7e165f8b4e8aeb46663ee12f3cca6ca
Sherzod <sherzodm8441@hstat.org>
2018-10-10 16:27:51 +0000

    ruby_basics

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,23 +1,25 @@
-def division
-    
+def division(num1, num2)
+    num1/num2
 end
 
-def argue
+def argue(word)
+    word
   
 end
 
-def greeting
-
+def greeting(greeting, name)
+    "#{greeting} #{name}"
 end
 
 def return_a_value
-    
+    "Nice"
 end
 
-def pizza_party
-    
+def pizza_party(thing="cheese")
+    thing
 end
 
-def whisper
+def whisper(word)
+    word.downcase
     
 end
